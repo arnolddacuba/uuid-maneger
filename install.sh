3e0e2c0f948bdb1720be619db733a314f07ee911
arnolddacuba <71627744+arnolddacuba@users.noreply.github.com>
2021-03-02 10:59:02 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt install jq -y
 
 wget -q https://raw.githubusercontent.com/arnolddacuba/uuid-maneger/main/magicvpn
 mv phcv2raymanager /usr/local/sbin/phcv2raymanager
-chmod +x /usr/local/sbin/phcv2raymanager
+chmod +x /usr/local/sbin/magicvpn
 clear
 echo "========================================"
 echo "use magicvpn to manage your UUID"
