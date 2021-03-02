460738bc8e0a6242bba350df77ce0b4e546be39b
arnolddacuba <71627744+arnolddacuba@users.noreply.github.com>
2021-03-02 10:04:26 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 apt update && apt upgrade -y -f
 apt install jq -y
 
-wget -q https://raw.githubusercontent.com/PHCitizen/v2ray-uuid-manager/main/phcv2raymanager
+wget -q https://raw.githubusercontent.com/arnolddacuba/uuid-maneger/main/magicvpn
 mv phcv2raymanager /usr/local/sbin/phcv2raymanager
 chmod +x /usr/local/sbin/phcv2raymanager
 clear
