80643963d3cdf06b22322b845d9e47209de30809
arnolddacuba <71627744+arnolddacuba@users.noreply.github.com>
2021-03-02 10:00:53 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+apt update && apt upgrade -y -f
+apt install jq -y
+
+wget -q https://raw.githubusercontent.com/PHCitizen/v2ray-uuid-manager/main/phcv2raymanager
+mv phcv2raymanager /usr/local/sbin/phcv2raymanager
+chmod +x /usr/local/sbin/phcv2raymanager
+clear
+echo "========================================"
+echo "use magicvpn to manage your UUID"
+echo "========================================"
